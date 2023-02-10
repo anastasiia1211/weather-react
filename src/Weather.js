6332ad3ad89e35b3074ca2acc3dfef0524168715
anastasiia1211 <119802989+anastasiia1211@users.noreply.github.com>
2023-02-10 16:41:19 +0100

    weather without forecast

--- src/Weather.js
+++ src/Weather.js
@@ -61,7 +61,6 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
